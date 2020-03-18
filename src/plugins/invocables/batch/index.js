5f162a1091bb742557d424ab6c1eb7613ce0f153
cesarvh <cesarv.h@berkeley.edu>
2020-03-18 11:26:26 -0700

    CC-1058/CC-1102: Remove ucbnh and pahma specific fields

--- src/plugins/invocables/batch/index.js
+++ src/plugins/invocables/batch/index.js
@@ -1,6 +1,6 @@
 import mergeAuthorityItemsBatchJob from './org.collectionspace.services.batch.nuxeo.MergeAuthorityItemsBatchJob';
 import updateInventoryStatusBatchJob from './org.collectionspace.services.batch.nuxeo.UpdateInventoryStatusBatchJob';
-import  bulkObjectEditBatchJob from './org.collectionspace.services.batch.nuxeo.BulkObjectEditBatchJob';
+import bulkObjectEditBatchJob from './org.collectionspace.services.batch.nuxeo.BulkObjectEditBatchJob';
 
 export default [
   mergeAuthorityItemsBatchJob,
